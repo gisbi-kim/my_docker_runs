eb64a96b945674cf2534ced47ef2eaa37296648f
Giseop Kim <gsk@dgist.ac.kr>
2025-01-30 23:21:06 +0900

    Update docker_run_gpu_mvdust3r.sh

--- docker_run_gpu_mvdust3r.sh
+++ docker_run_gpu_mvdust3r.sh
@@ -1,5 +1,7 @@
+#!/bin/bash
+
 # Docker 이미지 설정
-IMAGE="gsk1m/nvidia-cuda:12.0.0-devel-ubuntu22.04-mvdust3r"
+IMAGE="nvidia/cuda:12.0.0-devel-ubuntu22.04-mvdust3r"
 
 # 이미지가 로컬에 있는지 확인
 if ! docker images --format "{{.Repository}}:{{.Tag}}" | grep "$IMAGE"; then
@@ -14,16 +16,24 @@ else
     echo "이미지 $IMAGE 가 이미 로컬에 존재합니다."
 fi
 
-#!/bin/bash
-
 # 사용자로부터 워크디렉토리 입력 받기
+echo ""
 echo "작업 디렉토리를 입력하세요 (Enter를 누르면 기본 디렉토리 사용):"
 read WORKDIR
-
 # 기본 디렉토리 설정
 if [ -z "$WORKDIR" ]; then
     WORKDIR="/home/gskim/Documents/practices/mvdust3r"
-    echo "기본 디렉토리 사용: $WORKDIR"
+    echo "WORKDIR 기본 디렉토리 사용: $WORKDIR"
+fi
+
+# 사용자로부터 data디렉토리 입력 받기
+echo ""
+echo "Data 디렉토리를 입력하세요 (Enter를 누르면 기본 디렉토리 사용):"
+read DATADIR
+# 기본 디렉토리 설정
+if [ -z "$DATADIR" ]; then
+    DATADIR="/home/gskim/Documents/data/iphone/stray"
+    echo "DATADIR 기본 디렉토리 사용: $DATADIR"
 fi
 
 # 컨테이너 이름 설정
@@ -33,9 +43,10 @@ CONTAINER_NAME="mvdust3r"
 docker run --rm -it \
     --privileged \
     --gpus all \
+    -e CUDA_VISIBLE_DEVICES=0 \
     --name "$CONTAINER_NAME" \
     -v "$WORKDIR":/ws \
+    -v "$DATADIR":/data \
     -w /ws \
     --net=host \
-    "$IMAGE" \
-    /bin/bash -c "export PYTHONPATH=$PYTHONPATH:/ws/lib/python3.10/site-packages && python3 demo.py --weights ./checkpoints/MVDp_s2.pth"
+    "$IMAGE" \    /bin/bash -c "export PYTHONPATH=$PYTHONPATH:/ws/lib/python3.10/site-packages && python3 demo.py --weights ./checkpoints/MVDp_s2.pth; bash;"
